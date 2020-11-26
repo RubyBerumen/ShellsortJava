309ced3098aa91d771b47853e3840be7fb37c33b
Ruby Berumen <clararubipb15@gmail.com>
2020-11-25 20:04:14 -0600

    Se modificaron algunas variables

--- src/PruebaShellSort.java
+++ src/PruebaShellSort.java
@@ -3,35 +3,31 @@ import java.util.Arrays;
 class Shellsort{
 	
 	public void ordenar(int[] numeros) {
-		int salto,j,k,aux;
-		salto = numeros.length/2;
+		int intervalo;
+		intervalo = numeros.length/2;
 		
-		while(salto>0) {
-			for(int i = salto; i<numeros.length; i++) {
-				j=i-salto;
+		while(intervalo>0) {
+			for(int i = intervalo; i<numeros.length; i++) {
+				int j=i-intervalo;
 				while(j>=0) {
-					k=j+salto;
+					int k=j+intervalo;
 					if(numeros[j] <= numeros[k]) {
 						j-=1;
 					}else {
-						aux = numeros[j];
+						int aux = numeros[j];
 						numeros[j] = numeros[k];
 						numeros[k] = aux;
-						j-=salto;
+						j-=intervalo;
 					}
 				}
 			}
-			salto=salto/2;
+			intervalo=intervalo/2;
 		}
 		
 	}//public void ordenar
 	
-	
-	public void imprimir(int[] numeros) {
-		System.out.println(Arrays.toString(numeros));
-	}
-	
 }//class shellsort
+
 
 public class PruebaShellSort {
 
